9ddb762972cbffa6873f8a3bce21ef82b27687d3
UsuarioM <jjdalmedo@gmail.com>
2023-07-19 12:44:53 +0200

    cambios

---
+++ data/projects.js
@@ -0,0 +1,39 @@
+export const projects = [
+    {
+    title: "Fragata F-100",
+    description: "Militar",
+    image: /icons/fragata.png, 
+    tech: ["React", "HTML5", "CSS3", "JavaScript"],
+    github: "http://github.com/myuser/proyecto1",
+    link: "www.proyecto1.com",
+    },
+    {
+    title: "Granelero",
+    description: "Mercante",
+    image: /icons/granelero.png, 
+    tech: ["React", "HTML5", "CSS3", "JavaScript"], 
+    github: "http://github.com/myuser/proyecto2",
+    link: "www.proyecto1.com",
+    },
+    {
+    title: "Metanero",
+    description: "Mercante",
+    image: /icons/metanero.png,
+    tech: ["React", "HTML5", "CSS3", "JavaScript"],
+    github: "http://github.com/myuser/proyecto3",
+    link: "www.proyecto1.com",
+    },
+    {
+    title: "Petrolero",
+    description: "Mercante",
+    image: /icons/petrolero.png,
+    tech: ["React", "HTML5", "CSS3", "JavaScript"],
+    github: "http://github.com/myuser/proyecto4",
+    link: "www.proyecto1.com",
+    },
+    {title: "Portaaviones",
+    description: "Militar",
+    image: /icons/portaaviones.png,
+    tech: ["React", "HTML5", "CSS3", "JavaScript"],
+    }
+    ];
